b9d1ac844f06a9bdcfc098f313bd9ce05804388a
shuang <shuang.lixx@qq.com>
2023-05-29 19:28:03 +0800

    完成首屏开发

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Quote from "./sections/Quote";
 import { GlobalStyle } from "./styles/GlobalStyle"
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Quote/>
     </>
   );
 }
