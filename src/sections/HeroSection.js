7e53ddd4097aa6e8a49731bcccb6777752bfc389
shuang <shuang.lixx@qq.com>
2023-05-31 21:28:14 +0800

    适配手机端

--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -2,25 +2,37 @@ import React from "react";
 import styled from "styled-components";
 import backgroundVideo from '../assets/video/Ink - 21536.mp4';
 const Section = styled.section`
-width:100vw;
-height:100vh;
-position:relative;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
 
-display:flex;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 
-background-color:var(--dark);
-overflow:hidden; 
+  background-color: var(--dark);
+  overflow: hidden;
 `
+
 const Title = styled.h1`
-position:absolute;
-top:2rem;
-left:2rem;
+  position: absolute;
+  top: 2rem;
+  left: 2rem;
 
-font-size:var(--fontlg);
-font-family:var(--fontL);
-color:var(--greyLight);
+  font-size: var(--fontlg);
+  font-family: var(--fontL);
+  color: var(--greyLight);
+
+  @media screen and (max-width: 48em) {
+    font-size: var(--fontmd);
+    left: 1rem;
+  }
+
+  @media screen and (max-width: 30em) {
+    width: 70%;
+    color: var(--white);
+  }
 `
-
 const TextContainer = styled.div`
   width: 100%;
   height: 100vh;
@@ -33,12 +45,35 @@ const TextContainer = styled.div`
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  z-index:1;
-  span{
-    font-size:var(--fontxxxl);
-    text-transform:uppercase;
-    font-weight:600;
-    padding:2rem;
+  z-index: 1;
+
+  span {
+    font-size: var(--fontxxxl);
+    text-transform: uppercase;
+    font-weight: 600;
+    padding: 2rem;
+
+    @media screen and (max-width: 64em) {
+      font-size: var(--fontxxl);
+      padding: 0;
+    }
+    @media screen and (max-width: 48em) {
+      font-size: var(--fontxl);
+    }
+  }
+
+  @media screen and (max-width: 48em) {
+    flex-direction: column;
+    background-image: linear-gradient(90deg, var(--gradient));
+    align-items: flex-start;
+    filter: brightness(1.1);
+
+    & > *:last-child {
+      align-self: flex-end;
+    }
+
+    height: 80vh;
+    padding: 0 1rem;
   }
 `
 
